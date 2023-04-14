9661315e2d73f30db3fccbdf5fed25f1d592f7b7
johnmoormaniii <johnmoormaniii@gmail.com>
2023-04-14 10:02:28 -0400

    Added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 
 type Props = {};
 
@@ -8,7 +9,7 @@ export default function App({}: Props) {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
 
 type Props = {};
 
 export default function About({}: Props) {
   return (
-    <>
-      <main></main>
-    </>
+    <main className="min-h-screen w-full bg-gradient-to-b from-slate-800 via-slate-700 to-slate-800 flex flex-col items-center">
+      <Header />
+      <h1 className=" mt-32 text-3xl animate-bounce">
+        This is the "About" page, it's just showing the different routes!
+      </h1>
+    </main>
   );
 }
